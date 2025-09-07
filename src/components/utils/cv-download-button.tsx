56f13b8f14e68e547ee66bad125b8ed7061ef3fa
Tomáš Richtr <tomas.richtr@designeo.cz>
2025-09-07 20:32:44 +0200

    Update CV download link to Google Docs and remove unused `getAWSUrl` import.

--- src/components/utils/cv-download-button.tsx
+++ src/components/utils/cv-download-button.tsx
@@ -1,9 +1,6 @@
 "use client";
 
 import WithSkeleton from "@/components/layout/with-skeleton";
-import {
-  getAWSUrl,
-} from "@/utils/file";
 
 interface CvDownloadButtonProps {
   className?: string;
@@ -17,7 +14,7 @@ export const CvDownloadButton = ({
       widthClass="w-20"
     >
       <a
-        href={getAWSUrl("cv-tomas-richtr.pdf")}
+        href="https://docs.google.com/document/d/1jRGmHYF0PjFB-f_qe_xoX6RIKeB5Qz2HX8lCZqnvddk/edit?usp=sharing"
         download="CV-Tomas-Richtr.pdf"
         className={`btn btn-accent shadow-lg rounded-lg flex items-center gap-2 transition-transform duration-200 hover:scale-105 ${className}`}
         target="_blank"
