23cea70c2df189b6b7122032dcef4ea6fb1c6598
Tomáš Richtr <tomas.richtr@designeo.cz>
2026-04-15 09:41:08 +0200

    fix next image processing

---
+++ src/types/assets.d.ts
@@ -0,0 +1,71 @@
+declare module "*.png" {
+  const content: {
+    src: string;
+    height: number;
+    width: number;
+    blurDataURL?: string;
+    blurWidth?: number;
+    blurHeight?: number;
+  };
+  export default content;
+}
+
+declare module "*.jpg" {
+  const content: {
+    src: string;
+    height: number;
+    width: number;
+    blurDataURL?: string;
+    blurWidth?: number;
+    blurHeight?: number;
+  };
+  export default content;
+}
+
+declare module "*.jpeg" {
+  const content: {
+    src: string;
+    height: number;
+    width: number;
+    blurDataURL?: string;
+    blurWidth?: number;
+    blurHeight?: number;
+  };
+  export default content;
+}
+
+declare module "*.webp" {
+  const content: {
+    src: string;
+    height: number;
+    width: number;
+    blurDataURL?: string;
+    blurWidth?: number;
+    blurHeight?: number;
+  };
+  export default content;
+}
+
+declare module "*.gif" {
+  const content: {
+    src: string;
+    height: number;
+    width: number;
+    blurDataURL?: string;
+    blurWidth?: number;
+    blurHeight?: number;
+  };
+  export default content;
+}
+
+declare module "*.svg" {
+  const content: {
+    src: string;
+    height: number;
+    width: number;
+    blurDataURL?: string;
+    blurWidth?: number;
+    blurHeight?: number;
+  };
+  export default content;
+}
