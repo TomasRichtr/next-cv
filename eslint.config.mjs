7f9a87942a56d51c07cdeadb23909194fb385b52
Tomáš Richtr <tomas.richtr@designeo.cz>
2026-04-15 09:17:24 +0200

    fix eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,25 +1,11 @@
-import {
-  dirname,
-} from "path";
-import {
-  fileURLToPath,
-} from "url";
-
-import {
-  FlatCompat,
-} from "@eslint/eslintrc";
+import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
+import nextTypescript from "eslint-config-next/typescript";
 import stylistic from "@stylistic/eslint-plugin";
 import tseslint from "typescript-eslint";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...nextCoreWebVitals,
+  ...nextTypescript,
   ...tseslint.configs.recommended,
   {
     files: ["**/*.{js,jsx,ts,tsx}"],
